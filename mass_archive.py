ef88c2cc2a6ebacffb8c9d60458cd0f14a33a760
Nex <nex@nex.sx>
2018-09-03 19:24:38 +0200

    Added ability to archive URLs from a list, added argparse and Python 3 support

--- mass_archive.py
+++ mass_archive.py
@@ -1,68 +1,95 @@
+#!/usr/bin/env python3
+
+import os
 import sys
+import json
+import argparse
 import requests
 import archiveis
-import json
 
-target     = sys.argv[1]
-input      = str(target)
+# Modify this to add a default Perma.cc API key.
+perma_key = ""
 
-print "[*] Archiving %s..." % input
-
-#
-# function for pushing to The Internet Archive/Wayback Machine
-#
-def internet_archive(input):
+def internet_archive(url):
+    """
+    Function for pushing to The Internet Archive/Wayback Machine
+    """
+    print("[*] Pushing to the Wayback Machine...")
     
-    print "[*] Pushing to the Wayback Machine..."
-    
-    save_url = "https://web.archive.org/save/%s" % input
-    
-    # send off request to wayback machine
+    save_url = "https://web.archive.org/save/%s" % url
     response = requests.get(save_url)
     
     if response.status_code == 200:
-        
-        # grab the part of the URL dealing with the archive page
-        result               = response.headers['Content-Location']
-        
-        # build archive URL 
-        internet_archive_url = "https://web.archive.org%s" % result
-        
-        return internet_archive_url
-    else:
-        print "[!] Connection error"
+        result = response.headers["Content-Location"]
+        internet_archive_url = "https://web.archive.org{}".format(result)
 
-#
-# function for pushing to Perma.cc
-#   
-def perma(input):
-    
-    print "[*] Pushing to Perma.cc..."
-    
-    perma_json = {}
-    perma_json['url'] = '%s' % input
-    
-    # remember to put your Perma.cc API key in here
-    response = requests.post("https://api.perma.cc/v1/archives/?api_key=YOUR_PERMA_API_KEY_HERE", data=perma_json)
+        print(internet_archive_url)
+    else:
+        print("[!] Connection error")
+
+def perma(url):
+    """
+    Function for pushing to perma.cc
+    """
+    if not perma_key:
+        return
+
+    print("[*] Pushing to Perma.cc...")
+
+    perma_url = "https://api.perma.cc/v1/archives/?api_key={}".format(perma_key)
+    perma_json = {"url": url}
+    response = requests.post(perma_url, data=perma_json)
     if response.status_code == 201:
-         
         result = json.loads(response.content)
-        page_id = result['guid']
-        perma_url = "https://perma.cc/%s" % page_id
+        page_id = result["guid"]
+        archived_url = "https://perma.cc/{}".format(page_id)
 
-        return perma_url
+        print(archived_url)
     else:
-        print "[*] Connection error"
-    
-# push to The Internet Archive
-internet_archive_result = internet_archive(input)
-print internet_archive_result
+        print("[!] Connection error: did you provide a valid API key?")
 
-# push to archive.is
-print "[*] Pushing to archive.is..."
-archiveis_result = archiveis.capture(input).replace("http://", "https://")
-print archiveis_result
+def archive_is(url):
+    """
+    Function for pushing to archive.is
+    """
+    print("[*] Pushing to archive.is...")
+    archiveis_result = archiveis.capture(url).replace("http://", "https://")
+    print(archiveis_result)
 
-# push to perma.cc
-perma_result = perma(input)
-print perma_result
+def main():
+    parser = argparse.ArgumentParser(description="Archive a webpage on multiple online web archives")
+    parser.add_argument("--perma-key", action="store", required=False,
+                        help="Specify API key required to submit URL to Perma.cc")
+    parser.add_argument("--list", action="store_true", default=False, required=False,
+                        help="Enable this flag if the target is not a URL, but a file with list of URLs")
+    parser.add_argument("target", action="store",
+                        help="The URL you want to archive or the path to a list of URLs")
+    args = parser.parse_args()
+
+    if args.perma_key:
+        perma_key = args.perma_key
+
+    urls = []
+
+    if args.list:
+        if not os.path.exists(args.target):
+            print("[!] ERROR: the file you specified does not exist")
+            sys.exit(-1)
+
+        with open(args.target, "r") as handle:
+            for line in handle:
+                urls.append(line.strip())
+    else:
+        urls.append(args.target)
+
+    for url in urls:
+        print("[+] Archiving {}...".format(url))
+
+        for func in [internet_archive, perma, archive_is]:
+            try:
+                func(url)
+            except Exception as e:
+                print("[!] ERROR: {}".format(e))
+
+if __name__ == "__main__":
+    main()
